7f6d4a64fa84be4e47345d111b0fa7cbdd2a6a47
01Dri <drikill01@gmail.com>
2023-09-25 16:45:11 -0300

    Iniciando a aplicação

---
+++ src/main/java/me/dri/Catvie/CatvieApplication.java
@@ -0,0 +1,14 @@
+package me.dri.Catvie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatvieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatvieApplication.class, args);
+	}
+
+
+}
---
+++ src/test/java/me/dri/Catvie/CatvieApplicationTests.java
@@ -0,0 +1,13 @@
+package me.dri.Catvie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatvieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
